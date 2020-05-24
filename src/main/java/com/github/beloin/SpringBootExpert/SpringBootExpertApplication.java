26663345e9ec2ffe71e3f9b999ec8dcdbab7f0a2
Beloin Sena <beloin.rodrigues@gmail.com>
2020-05-24 15:54:42 -0300

    convertendo para JPA

--- src/main/java/com/github/beloin/SpringBootExpert/SpringBootExpertApplication.java
+++ src/main/java/com/github/beloin/SpringBootExpert/SpringBootExpertApplication.java
@@ -1,5 +1,7 @@
 package com.github.beloin.SpringBootExpert;
 
+import com.github.beloin.SpringBootExpert.domain.entity.Cliente;
+import com.github.beloin.SpringBootExpert.domain.repository.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +12,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootExpertApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+            clientes.salvarCliente(new Cliente("Sena Extreme"));
+            clientes.salvarCliente(new Cliente("Daff Daffinha"));
+
+            List<Cliente> cli = clientes.obterTodos();
+            cli.forEach(System.out::println);
+
+            cli.forEach(c -> {
+                c.setNome(c.getNome() + " Atualizado");
+                clientes.atualizar(c);
+            });
+
+            cli = clientes.obterTodos();
+            cli.forEach(System.out::println);
+
+            clientes.deletar(clientes.buscarPorNome("Se").get(0));
+
+            cli = clientes.obterTodos();
+            cli.forEach(System.out::println);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootExpertApplication.class, args);
     }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+create table produto(
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(100),
+    preco_unitario NUMERIC(20,2)
+);
+
+create table cliente (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100)
+);
+
+
+create table pedido(
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    cliente_id integer REFERENCES cliente (id),
+    data_pedido TIMESTAMP,
+    total NUMERIC(20,2)
+);
+
+create table item_pedido(
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    pedido_id integer REFERENCES pedido (id),
+    produto_id integer REFERENCES produto (id),
+    quantidade integer
+);
